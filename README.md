43c54b0902b1970d313b9e525b7e7305ea6cce96
briri <brian.riley@ucop.edu>
2021-02-05 15:28:19 -0800

    initial test script

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # v1_api_tester
-A script that allows you to test the V1 API
+
+This script can be used to verify that the V1 API of a DMPRoadmap system is functioning and configured properly.
+
+## Testing the API
+
+This script reqires that you have an instance of the DMPRoadmap system running locally or on a server that you have access to.
+
+Please note that it only tests the API as an ApiClient (e.g. an external system) not an individual User.
+
+To run it you must pass in 3 arguments: the host, your client id and your client secret.
+For example: `ruby dmproadmap_api_tester.rb http://localhost:3000 1234567890 0987654321`
+
+If you do not yet have an API Client, you can create one by logging into the DMPRoadmap system as a SuperAdmin and then navigating to the 'Api Clients' page via the 'Admin' menu. The system will auto-generate your client_id and client_secret once the record has been created.
+
+## Further reading
+
+See the [API V1 Documentation](https://github.com/DMPRoadmap/roadmap/wiki/API-Documentation-V1) for more info on how to use the API.
+
---
+++ dmproadmap_api_tester.rb
@@ -0,0 +1,89 @@
+require 'httparty'
+
+HOST = ARGV[0]
+CLIENT_ID = ARGV[1]
+CLIENT_SECRET = ARGV[2]
+DEFAULT_HEADERS = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+  'Server-Agent': "dmptool (#{CLIENT_ID})"
+}
+
+def retrieve_auth_token
+  payload = {
+    grant_type: 'client_credentials',
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET
+  }
+  target = "#{HOST}/api/v1/authenticate"
+
+  p "Authentication attempt:"
+  p "    Target: #{target}"
+  p "    Data: #{payload}"
+  p "    Headers: #{DEFAULT_HEADERS}"
+  p "-----------------------"
+  p
+
+  resp = HTTParty.post(target, body: payload.to_json, headers: DEFAULT_HEADERS, debug: true)
+  response = JSON.parse(resp.body)
+
+  p "Unable to authenticate: #{resp.code} - #{response.inspect}" unless resp.code == 200
+  return nil unless resp.code == 200
+
+  token = response
+  {
+    'Authorization': "#{token['token_type']} #{token['access_token']}"
+  }
+end
+
+def retrieve_templates(token:, page: 1)
+  target = "#{HOST}/api/v1/templates?page=#{page}"
+  headers = DEFAULT_HEADERS.merge(token)
+
+  p "Template listing attempt:"
+  p "    Target: #{target}"
+  p "    Headers: #{headers}"
+  p "-----------------------"
+  p
+
+  resp = HTTParty.get(target, headers: headers, debug: true)
+  p "Unable to get templates: #{resp.code} - #{resp.body}" unless resp.code == 200
+  resp.code == 200 ? JSON.parse(resp.body) : nil
+end
+
+def retrieve_plans(token:, page: 1)
+  target = "#{HOST}/api/v1/plans?page=#{page}"
+  headers = DEFAULT_HEADERS.merge(token)
+
+  p "Plan/DMP listing attempt:"
+  p "    Target: #{target}"
+  p "    Headers: #{headers}"
+  p "-----------------------"
+  p
+
+  resp = HTTParty.get(target, headers: headers, debug: true)
+  p "Unable to get plans: #{resp.code} - #{resp.body}" unless resp.code == 200
+  resp.code == 200 ? JSON.parse(resp.body) : nil
+end
+
+if ARGV.any? && ARGV.length == 3
+  # Authenticate
+  token = retrieve_auth_token
+  if token.is_a?(Hash)
+    p "TOKEN:"
+    pp token.inspect
+    p ''
+
+    # Retrieve the public templates
+    pp retrieve_templates(token: token).inspect
+    p ''
+
+    # Retrieve page 2 of the public templates
+    pp retrieve_plans(token: token).inspect
+
+    # You can then add further tasks
+  end
+else
+  p "Missing essential information. This script requires 3 arguments. The host (e.g. https://my.org.edu), your client_id and the client_secret."
+  p "Please retry with `ruby dmproadmap_api_tester.rb http://localhost:3000 12345 abcdefg`"
+end
